6c0991d2ff2c3f9822a257ae63999015b6695505
applepc12138 <32706153+applepc12138@users.noreply.github.com>
2020-03-16 21:53:53 +0800

    初步实现priority_queue

--- MySTL/mystl_queue.h
+++ MySTL/mystl_queue.h
@@ -2,14 +2,16 @@
 #ifndef STL_QUEUE
 #define STL_QUEUE
 #include "mystl_list.h"
+#include "mystl_vector.h"
+#include "mystl_iterator.h"
+#include <functional>
 
 namespace mystl {
 
 	template <typename T, typename Container = list<T>>
 	class queue {
-		friend bool operator== <T, Container> (const queue& x, const queue& y);
-		friend bool operator< <T, Container> (const queue& x, const queue& y);
-
+		friend bool operator== <T, Container> (const queue<T, Container>& x, const queue<T, Container>& y);
+		friend bool operator< <T, Container> (const queue<T, Container>& x, const queue<T, Container>& y);
 	private:
 		Container c;
 	public:
@@ -33,9 +35,6 @@ namespace mystl {
 		
 		void push(const value_type& v) { c.push_back(v); }
 		void pop() { c.pop_front(); }
-
-
-
 	};
 
 	template <typename T, typename Container /*= list<T>*/>
@@ -49,6 +48,134 @@ namespace mystl {
 	{
 		return x.c < y.c;
 	}
+
+
+/*************************************************************************************************/
+
+	template <class RandomAccessIterator, class Distance, class T, class Compare>
+	void __push_heap(RandomAccessIterator first, Distance holeIndex,
+		Distance topIndex, T value, Compare comp) {
+		Distance parent = (holeIndex - 1) / 2;
+		while (holeIndex > topIndex && comp(*(first + parent), value)) {
+			*(first + holeIndex) = *(first + parent);
+			holeIndex = parent;
+			parent = (holeIndex - 1) / 2;
+		}
+		*(first + holeIndex) = value;
+	}
+
+	template <class RandomAccessIterator, class Compare>
+	inline void push_heap(RandomAccessIterator first, RandomAccessIterator last,
+		Compare comp = std::less<typename iterator_traits<RandomAccessIterator>::value_type>()) {
+		typedef typename iterator_traits<RandomAccessIterator>::difference_type  Distance;
+		typedef typename iterator_traits<RandomAccessIterator>::value_type Value_type;
+		__push_heap(first, Distance((last - first) - 1), Distance(0), Value_type(*(last - 1)), comp);
+	}
+
+	template <class RandomAccessIterator, class Distance, class T, class Compare>
+	void __adjust_heap(RandomAccessIterator first, Distance holeIndex,
+		Distance len, T value, Compare comp) {
+		Distance topIndex = holeIndex;
+		Distance secondChild = 2 * holeIndex + 2;
+		while (secondChild < len) {
+			if (comp(*(first + secondChild), *(first + (secondChild - 1))))
+				secondChild--;
+			*(first + holeIndex) = *(first + secondChild);
+			holeIndex = secondChild;
+			secondChild = 2 * (secondChild + 1);
+		}
+		if (secondChild == len) {
+			*(first + holeIndex) = *(first + (secondChild - 1));
+			holeIndex = secondChild - 1;
+		}
+		__push_heap(first, holeIndex, topIndex, value, comp);
+	}
+
+	template <class RandomAccessIterator, class T, class Compare, class Distance>
+	inline void __pop_heap(RandomAccessIterator first, RandomAccessIterator last,
+		RandomAccessIterator result, T value, Compare comp,
+		Distance*) {
+		*result = *first;
+		__adjust_heap(first, Distance(0), Distance(last - first), value, comp);
+	}
+
+	template <class RandomAccessIterator, class Compare>
+	inline void pop_heap(RandomAccessIterator first, RandomAccessIterator last,
+		Compare comp = std::less<typename iterator_traits<RandomAccessIterator>::value_type>()) {
+		typedef typename iterator_traits<RandomAccessIterator>::difference_type  Distance;
+		typedef typename iterator_traits<RandomAccessIterator>::value_type Value_type;
+		__pop_heap(first, last - 1, last - 1, Value_type(*(last - 1)), comp, (Distance*)(0));
+	}
+
+
+	template <class RandomAccessIterator, class Compare, class T, class Distance>
+	void __make_heap(RandomAccessIterator first, RandomAccessIterator last,
+		Compare comp, T*, Distance*) {
+		if (last - first < 2) return;
+		Distance len = last - first;
+		Distance parent = (len - 2) / 2;
+
+		while (true) {
+			__adjust_heap(first, parent, len, T(*(first + parent)), comp);
+			if (parent == 0) return;
+			parent--;
+		}
+	}
+
+	template <class RandomAccessIterator, class Compare>
+	inline void make_heap(RandomAccessIterator first, RandomAccessIterator last,
+		Compare comp = std::less<typename iterator_traits<RandomAccessIterator>::value_type>()) {
+		typedef typename iterator_traits<RandomAccessIterator>::difference_type  Distance;
+		typedef typename iterator_traits<RandomAccessIterator>::value_type Value_type;
+		if (last - first < 2) 
+			return;
+		Distance len = last - first;
+		Distance parent = (len - 2) / 2;
+
+		while (true) {
+			__adjust_heap(first, parent, len, Value_type(*(first + parent)), comp);
+			if (parent == 0) return;
+			parent--;
+		}
+	}
+
+	template <class RandomAccessIterator, class Compare>
+	void sort_heap(RandomAccessIterator first, RandomAccessIterator last,
+		Compare comp = std::less<typename iterator_traits<RandomAccessIterator>::value_type>()) {
+		while (last - first > 1) 
+			pop_heap(first, last--, comp);
+	}                          
+
+	template <typename T, typename Container = vector<T>, typename Comp = std::less<T>>
+	class priority_queue {
+	private:
+		Container c;
+		Comp comp;
+	public:
+		typedef typename Container::value_type value_type;
+		typedef typename Container::size_type size_type;
+		typedef typename Container::reference reference;
+		typedef typename Container::const_reference const_reference;
+
+	public:
+		priority_queue() = default;
+		template <typename InputIterator>
+		priority_queue(InputIterator first, InputIterator last) : c(first, last) {
+			make_heap(c.begin(), c.end(), comp);
+		}
+		bool empty()const { return c.empty(); }
+		size_type size()const { return c.size(); }
+		const_reference top()const { return c.front(); }
+		void push(const value_type& x) {
+			c.push_back(x);
+			push_heap(c.begin(), c.end(), comp);
+		}
+		void pop() {
+			pop_heap(c.begin(), c.end(), comp);
+			c.pop_back();
+		}
+	};
+
 }
 
 #endif // !STL_QUEUE
